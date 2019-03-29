06bfe1893de31684f7403f08077c3ef6c8516601
Fei Liu <liu.feiwood@gmail.com>
2019-03-29 11:03:46 +0100

    add material ui and styled-componetns

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
 import Dom from "react-dom";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import "./main.css";
+import { WINDOW_HEIGHT, WINDOW_WIDTH, padding } from "./style";
 
 interface PropTypes {
   msg: string;
 }
 
+const Container = styled.div`
+  overflow: hidden;
+  width: ${WINDOW_WIDTH}px;
+  height: ${WINDOW_HEIGHT}px;
+  padding: ${padding.standard}px;
+  box-sizing: border-box;
+`;
+
 const App = (props: PropTypes) => (
-  <p>Parcel browser extension in typescript {props.msg}</p>
+  <Container>
+    <Button variant="contained" color="primary">
+      Hello
+    </Button>
+    <p>Parcel browser extension in typescript {props.msg}</p>
+  </Container>
 );
 
 export default (): void => {
---
+++ src/style.ts
@@ -0,0 +1,5 @@
+export const WINDOW_WIDTH = 300;
+export const WINDOW_HEIGHT = 600;
+export const padding = {
+  standard: 16
+};
