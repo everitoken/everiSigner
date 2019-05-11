78e1f1f2201cbd92f03c30a5e2a345d2d5e9f21f
Fei Liu <liu.feiwood@gmail.com>
2019-05-11 20:16:01 +0200

    add error dialog

--- src/ui/presentational/AccountNameComponent.tsx
+++ src/ui/presentational/AccountNameComponent.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
-import { FormControl, InputLabel, Input, Button } from '@material-ui/core'
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  Typography,
+} from '@material-ui/core'
 import FlexContainer from './FlexContainer'
+import AlertDialog from './AlertDialog'
 
 type PropTypes = {
   autoFocus?: boolean
@@ -10,14 +17,14 @@ type PropTypes = {
 }
 type StateProps = {
   accountName: string
-  invalid: boolean
+  errorMessage: string
 }
 
 class AccountNameComponent extends React.PureComponent<PropTypes, StateProps> {
   static defaultState = { autoFocus: false }
   state = {
     accountName: '',
-    invalid: false,
+    errorMessage: '',
   }
 
   handleAccountChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,64 +33,86 @@ class AccountNameComponent extends React.PureComponent<PropTypes, StateProps> {
   }
   isValid = () => {
     if (this.props.accountNames.includes(this.state.accountName)) {
-      return false
+      return {
+        isValid: false,
+        errorMessage: `Account name: "${
+          this.state.accountName
+        }" already exists.`,
+      }
     }
     if (
       this.state.accountName.length === 0 ||
       this.state.accountName.length > 25
     ) {
-      return false
+      return {
+        isValid: false,
+        errorMessage: 'Account name exceeds max length.',
+      }
     }
 
-    return true
+    return { isValid: true, errorMessage: '' }
   }
   handleClick = () => {
-    const isValid = this.isValid()
+    const { isValid, errorMessage } = this.isValid()
+
+    this.setState({ errorMessage })
 
     if (!isValid) {
-      this.setState({ invalid: true })
       return
     }
-
-    this.setState({ invalid: false })
 
     this.props.onNextClick(this.state.accountName)
   }
   render() {
     return (
-      <FlexContainer>
-        <FlexContainer alignItems="stretch" justifyContent="center" withPadding>
-          <FormControl>
-            <InputLabel htmlFor="account-name">
-              Specify account name (less than 25 chars)
-            </InputLabel>
-            <Input
-              autoFocus={this.props.autoFocus}
-              style={{ fontSize: 24 }}
-              error={this.state.invalid}
-              id="account-name"
-              value={this.state.accountName}
-              onChange={this.handleAccountChange}
-            />
-          </FormControl>
-        </FlexContainer>
-        <div style={{ alignSelf: 'stretch' }}>
+      <React.Fragment>
+        <AlertDialog
+          title="Error"
+          open={this.state.errorMessage.length !== 0}
+          onClose={() => {
+            this.setState({ errorMessage: '' })
+          }}
+        >
+          <Typography>{this.state.errorMessage}</Typography>
+        </AlertDialog>
+        <FlexContainer>
           <FlexContainer
-            withPadding
             alignItems="stretch"
-            justifyContent="flex-end"
+            justifyContent="center"
+            withPadding
           >
-            <Button
-              variant="contained"
-              color="primary"
-              size="large"
-              onClick={this.handleClick}
-            >
-              {this.props.buttonText}
-            </Button>
+            <FormControl>
+              <InputLabel htmlFor="account-name">
+                Specify account name (less than 25 chars)
+              </InputLabel>
+              <Input
+                autoFocus={this.props.autoFocus}
+                style={{ fontSize: 24 }}
+                error={this.state.errorMessage.length !== 0}
+                id="account-name"
+                value={this.state.accountName}
+                onChange={this.handleAccountChange}
+              />
+            </FormControl>
           </FlexContainer>
-        </div>
-      </FlexContainer>
+          <div style={{ alignSelf: 'stretch' }}>
+            <FlexContainer
+              withPadding
+              alignItems="stretch"
+              justifyContent="flex-end"
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                onClick={this.handleClick}
+              >
+                {this.props.buttonText}
+              </Button>
+            </FlexContainer>
+          </div>
+        </FlexContainer>
+      </React.Fragment>
     )
   }
 }
--- src/ui/presentational/AlertDialog.tsx
+++ src/ui/presentational/AlertDialog.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogTitle from '@material-ui/core/DialogTitle'
-
+import {
+  withStyles,
+  Typography,
+  IconButton,
+  StyledComponentProps,
+} from '@material-ui/core'
+import MuiDialogTitle from '@material-ui/core/DialogTitle'
+import MuiDialogContent from '@material-ui/core/DialogContent'
+import MuiDialogActions from '@material-ui/core/DialogActions'
+import CloseIcon from '@material-ui/icons/Close'
 type PropTypes = {
   title: string
   children: React.ReactNode
@@ -12,7 +18,58 @@ type PropTypes = {
   onClose: () => void
 }
 
-class AlertDialog extends React.Component<PropTypes, { open: boolean }> {
+const DialogTitle = withStyles(theme => ({
+  root: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    margin: 0,
+    padding: theme.spacing.unit * 2,
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing.unit,
+    top: theme.spacing.unit,
+    color: theme.palette.grey[500],
+  },
+}))(
+  (
+    props: {
+      onClose: () => void
+      children: string
+    } & StyledComponentProps
+  ) => {
+    const { children, classes, onClose } = props
+    return (
+      <MuiDialogTitle disableTypography className={classes && classes.root}>
+        <Typography variant="h6">{children}</Typography>
+        {onClose ? (
+          <IconButton
+            aria-label="Close"
+            onClick={onClose}
+            className={classes && classes.closeButton}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </MuiDialogTitle>
+    )
+  }
+)
+const DialogContent = withStyles(theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing.unit * 3,
+  },
+}))(MuiDialogContent)
+
+const DialogActions = withStyles(theme => ({
+  root: {
+    // borderTop: `1px solid ${theme.palette.divider}`,
+    margin: 0,
+    padding: theme.spacing.unit,
+  },
+}))(MuiDialogActions)
+
+class AlertDialog extends React.Component<PropTypes> {
   render() {
     return (
       <Dialog
@@ -24,11 +81,17 @@ class AlertDialog extends React.Component<PropTypes, { open: boolean }> {
         onClose={this.props.onClose}
         aria-labelledby="alert-dialog-title"
       >
-        <DialogTitle id="alert-dialog-title">{this.props.title}</DialogTitle>
+        <DialogTitle onClose={this.props.onClose}>
+          {this.props.title}
+        </DialogTitle>
         <DialogContent>{this.props.children}</DialogContent>
         <DialogActions>
-          <Button onClick={this.props.onClose} color="primary">
-            Confirm and dismiss
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={this.props.onClose}
+          >
+            Cancel
           </Button>
         </DialogActions>
       </Dialog>
